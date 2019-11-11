c7865f022539df216382fa12129209ec5eac071c
sivvish <vishal.2015@iitg.ac.in>
2019-11-11 10:47:19 +0530

    made some more changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,6 +32,7 @@ const routes: Routes = [
   { path: 'update-product', component: UpdateProductComponent },
   { path: 'app-incart-products',component:IncartProductsComponent},
   //{ path: 'payment', component: PaymentComponent},
+  
   // Routes to be placed at the last
   { path: '', component: HomeComponent },
   { path: '**', component: PageNotFoundComponent }
