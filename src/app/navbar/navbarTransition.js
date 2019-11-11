b4d10ffdbb0a54c8821fc420de540b6ef20b2a4b
sivvish <vishal.2015@iitg.ac.in>
2019-11-11 10:04:27 +0530

    commented the navbar.js

--- src/app/navbar/navbarTransition.js
+++ src/app/navbar/navbarTransition.js
@@ -1,10 +1,10 @@
-var winEle = $(window);
-var nav = $('#main-nav');
+// var winEle = $(window);
+// var nav = $('#main-nav');
 
-winEle.scroll(function(){
-  if (winEle.scrollTop() > 50) {
-    nav.addClass('smallnav');
-  } else {
-  	nav.removeClass('smallnav');
-  }
-});	
+// winEle.scroll(function(){
+//   if (winEle.scrollTop() > 50) {
+//     nav.addClass('smallnav');
+//   } else {
+//   	nav.removeClass('smallnav');
+//   }
+// });	
